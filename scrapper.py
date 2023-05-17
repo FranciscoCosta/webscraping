e176ea2aea9e5134d30f03de837e0eceb59585c7
Xico <francisco100eg@gmail.com>
2023-05-17 17:49:00 -0300

    :Feat: Scraped all data from site

--- scrapper.py
+++ scrapper.py
@@ -15,7 +15,31 @@ page_content = page_contet_bytes.decode('utf-8')
 soup = BeautifulSoup(page_content, 'html.parser')
 
 # Pegando a tabela de pokemons
-# print(soup.prettify())
+
+pokemons_row = soup.find_all('table', id='pokedex')[0].find_all('tbody')[0].find_all('tr')
+soup.find
+for pokemon in pokemons_row:
+    pokemon_td = pokemon.find_all('td')
+    id = pokemon_td[0]['data-sort-value']
+    img = pokemon_td[0].find("img")["src"]
+    name = pokemon_td[1].find("a", class_="ent-name").text
+    if pokemon_td[1].find_all("small"):
+        name = pokemon_td[1].find_all("small")[0].text
+
+    description_url = pokemon_td[1].find_all("a")[0]["href"]
+    types = []
+    for type in pokemon_td[2].find_all("a"):
+        types.append(type.text)
+
+    total = pokemon_td[3].text
+    hp = pokemon_td[4].text
+    attack = pokemon_td[5].text
+    defense = pokemon_td[6].text
+    sp_atk = pokemon_td[7].text
+    sp_def = pokemon_td[8].text
+    speed = pokemon_td[9].text
+
+    print(id, img, name, description_url, types, total, hp, attack, defense, sp_atk, sp_def, speed)
 
 
 
